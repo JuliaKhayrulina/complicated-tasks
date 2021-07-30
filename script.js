c927d328a8a1587a6f6bd83e60d4ecc704e3a21e
Julia Khayrulina <julia748@mail.ru>
2021-07-30 18:01:53 +0300

    Complete the assignment for lesson 2
    1) Create variable num with value 266219 (data type number)
    2) Output to the console the product (multiplication) of the digits of this number
    3) The result is raised to power 3 using only 1 operator (Math.pow is not suitable)
    4) Output to the console the first 2 digits of the received number

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+let num = 266219;
+let result = String(num)
+  .split('')
+  .reduce((acc, c) => acc * c);
 
+console.log(result);
+
+result **= 3;
+console.log(result);
+console.log(String(result).substring(0, 2));

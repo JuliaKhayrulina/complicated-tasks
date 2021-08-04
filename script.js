202b8f2da1ea50a43f378a760104acb830f36413
Julia Khayrulina <julia748@mail.ru>
2021-08-04 11:43:40 +0300

    fix: remove the output to the console if the data is not a string

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const str =
-  'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae cumque reprehenderit aut nam repudiandae itaque laborum officia corrupti, sed placeat. Iure nostrum quo cum aut debitis suscipit laboriosam laudantium maiores ipsam quam, animi dignissimos rerum libero natus vitae fuga itaque asperiores? Vitae, maxime. Voluptate, necessitatibus!';
+// const data =
+//   '                                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae cumque reprehenderit aut nam repudiandae itaque laborum officia corrupti, sed placeat. Iure nostrum quo cum aut debitis suscipit laboriosam laudantium maiores ipsam quam, animi dignissimos rerum libero natus vitae fuga itaque asperiores? Vitae, maxime. Voluptate, necessitatibus!';
 
-//const str = 10;
+const data = 10;
 
-let result = getString(str);
+let result;
+getString(data);
 
 function getString(str) {
   if (typeof str != 'string') {
@@ -14,8 +15,12 @@ function getString(str) {
       str = str.substr(0, 30);
       str += '...';
     }
+    result = str;
+    showResult(data, result);
   }
-  return str;
 }
-console.log(`длина строки была: ${str.length} \n${str}`);
-console.log(`длина строки стала:${result.length} \n${result}`);
+
+function showResult(str, res) {
+  console.log(`длина строки была: ${str.length} \n${str}`);
+  console.log(`длина строки стала:${result.length} \n${result}`);
+}

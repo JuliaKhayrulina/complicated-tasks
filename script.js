d4b52f5333891370910de8b49d42dcf2f1a349fe
Julia Khayrulina <julia748@mail.ru>
2021-08-07 01:48:57 +0300

    Create a week array and write the days of the week as strings to it
    - display all days of the week
    - each of them with a new line
    - weekends - in italics
    - current day - in bold (use date object)

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-const arr = ['3322', '2770', '8935', '4332', '9000', '2566', '4770'];
+const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+const today = new Date();
 
-arr.forEach((item) => {
-  if (item[0] == 2 || item[0] == 4) {
-    console.log(item);
-  }
-});
+//===============================================
 
-//=============================================
+let ul = document.createElement('ul');
+document.body.appendChild(ul);
 
-for (let i = 2; i <= 100; i++) {
-  let count = 0;
-  for (let j = 1; j <= 100; j++) {
-    if (i % j == 0) {
-      count += 1;
-    }
-  }
-  if (count <= 2) {
-    console.log(`${i} - делители этого числа: 1 и ${i}`);
+for (let i = 0; i < week.length; i++) {
+  let li = document.createElement('li');
+  ul.appendChild(li);
+  li.innerHTML = week[i];
+
+  if (i === +today.getDay() - 1) {
+    li.innerHTML = `<b>${week[i]}</b>`;
+  } else if (i == 5 || i == 6) {
+    li.innerHTML = `<i>${week[i]}</i>`;
   }
 }

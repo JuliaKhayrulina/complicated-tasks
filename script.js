392a45f85b3cfbda25e2ee6b0c0f3a4502cee10b
Julia Khayrulina <julia748@mail.ru>
2021-08-09 16:08:07 +0300

    Display the date and time according to the task:
    1) display the current date and time in 2 formats
    2) to output in the format (a) write a function that will change the declension of words depending on the number
    3) to output in the format (b) write a function that will add 0 before values that consist of one digit (from 9:5:3 1.6.2019 will do 09:05:03 01.06.2019)
    4) Using the setInterval function, implement date and time update every second

--- script.js
+++ script.js
@@ -1,19 +1,68 @@
-const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-const today = new Date();
+'use strict';
 
-//===============================================
+const days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+const months = [
+  'Января',
+  'Февраля',
+  'Марта',
+  'Апреля',
+  'Мая',
+  'Июня',
+  'Июля',
+  'Августа',
+  'Сентября',
+  'Октября',
+  'Ноября',
+  'Декабря',
+];
+const arrMinutes = ['минута', 'минуты', 'минут'];
+const arrHours = ['час', 'часа', 'часов'];
+const arrSeconds = ['секунда', 'секунды', 'секунд'];
 
-let ul = document.createElement('ul');
-document.body.appendChild(ul);
+//=========Окончание слов=======================
 
-for (let i = 0; i < week.length; i++) {
-  let li = document.createElement('li');
-  ul.appendChild(li);
-  li.innerHTML = week[i];
-
-  if (i === +today.getDay() - 1) {
-    li.innerHTML = `<b>${week[i]}</b>`;
-  } else if (i == 5 || i == 6) {
-    li.innerHTML = `<i>${week[i]}</i>`;
+function declOfWorlds(n, text) {
+  n = Math.abs(n) % 100;
+  let n1 = n % 10;
+  if (n > 10 && n < 20) {
+    return text[2];
   }
+  if (n1 > 1 && n1 < 5) {
+    return text[1];
+  }
+  if (n1 == 1) {
+    return text[0];
+  }
+  return text[2];
 }
+
+function upDate() {
+  const myDate = new Date();
+  const dayWeek = `Сегодня ${days[myDate.getDay()]},`;
+  const nowDate = `${myDate.getDate()} ${months[myDate.getMonth()]} ${myDate.getFullYear()} года,`;
+  const hours = myDate.getHours();
+  const minutes = myDate.getMinutes();
+  const seconds = myDate.getSeconds();
+  const time = `${hours} ${declOfWorlds(hours, arrHours)} ${minutes} ${declOfWorlds(
+    minutes,
+    arrMinutes
+  )}
+ ${seconds} ${declOfWorlds(seconds, arrSeconds)}`;
+  console.log(`${dayWeek} ${nowDate} ${time}`);
+}
+
+//======================================================================================
+
+function upTime() {
+  const myDate = new Date();
+  const day = ('0' + myDate.getDate()).slice(-2);
+  const month = ('0' + (myDate.getMonth(12) + 1)).slice(-2);
+  const hours = ('0' + myDate.getHours()).slice(-2);
+  const minutes = ('0' + myDate.getMinutes()).slice(-2);
+  const seconds = ('0' + myDate.getSeconds()).slice(-2);
+
+  console.log(`${day}.${month}.${myDate.getFullYear()} - ${hours}:${minutes}:${seconds}`);
+}
+
+setInterval(() => upDate(), 1000);
+setInterval(() => upTime(), 1000);

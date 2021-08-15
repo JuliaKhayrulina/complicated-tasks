36d508c3755e3aa7a74f81754ad7593ac2c1e9fd
Julia Khayrulina <julia748@mail.ru>
2021-08-15 14:00:09 +0300

    Make a DomElement class that contains properties and methods

--- script.js
+++ script.js
@@ -1,13 +1,46 @@
 'use strict';
 
-document.querySelector('button').addEventListener('click', () => {
-  let str = '';
-  for (let i = 0; i < 7; i++) {
-    let num = Math.ceil(Math.random() * (9 - 1) + 1);
-    str += num;
+class DomElement {
+  constructor(selector, height, width, bg, fontSize) {
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
   }
-  let toNumber = +str;
-  let color = toNumber.toString(16);
-  document.querySelector('p').textContent = color;
-  document.body.style.backgroundColor = `#${color}`;
-});
+  createElement() {
+    this.selector = this.selector.trim();
+    let element;
+
+    if (this.selector[0] === '.') {
+      element = document.createElement('div');
+      element.classList.add(`${this.selector}`);
+      element.textContent = `Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Ad, dolores nihil dolorum optio eligendi modi commodi facere vero omnis illum.`;
+      document.body.append(element);
+      this.createStyles(element);
+    }
+    if (this.selector[0] === '#') {
+      element = document.createElement('p');
+      element.id = `${this.selector}`;
+      element.textContent = `Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Ad, dolores nihil dolorum optio eligendi modi commodi facere vero omnis illum.`;
+      document.body.append(element);
+      this.createStyles(element);
+    }
+  }
+  createStyles(element) {
+    element.style.cssText = `
+    height: ${this.height}px;
+    width: ${this.width}px;
+    background: ${this.bg};
+    font-size: ${this.fontSize}px;
+  `;
+  }
+}
+
+const newElem = new DomElement('        #paragraph', 'auto', 500, '#bbe46a', 30);
+const newElem2 = new DomElement('        .block', 'auto', 300, 'rgb(228 106 222 / 50%)', 16);
+
+newElem.createElement();
+newElem2.createElement();

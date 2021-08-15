8e796465ab4785d637b2de40dc7f905026cf33f8
Julia Khayrulina <julia748@mail.ru>
2021-08-15 15:59:30 +0300

    Using class DomElement from the main task #1, create a square of 100 by 100 pixels.
    1) the square must be set to background of any color and the position property: absolute
    2) place it on the page only after the DOMContentLoaded event is executed
    3) write an event handler for the keydown, which will accept the callback function. This function will catch pressing the arrows of the keyboard. Depending on the button pressed (Up - Up Arrow, Left - Left Arrow, Right - Right Arrow, Down - Down Arrow) our square will move by 10 pixels

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 'use strict';
 
 class DomElement {
-  constructor(selector, height, width, bg, fontSize) {
+  constructor(selector, height, width, bg, fontSize, text, objCss) {
     this.selector = selector;
     this.height = height;
     this.width = width;
     this.bg = bg;
     this.fontSize = fontSize;
+    this.text = text;
+    this.objCss = objCss;
   }
   createElement() {
     this.selector = this.selector.trim();
@@ -15,32 +17,63 @@ class DomElement {
     if (this.selector[0] === '.') {
       element = document.createElement('div');
       element.classList.add(`${this.selector}`);
-      element.textContent = `Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-      Ad, dolores nihil dolorum optio eligendi modi commodi facere vero omnis illum.`;
+      element.textContent = this.text;
       document.body.append(element);
       this.createStyles(element);
     }
     if (this.selector[0] === '#') {
       element = document.createElement('p');
       element.id = `${this.selector}`;
-      element.textContent = `Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-      Ad, dolores nihil dolorum optio eligendi modi commodi facere vero omnis illum.`;
+      element.textContent = this.text;
       document.body.append(element);
       this.createStyles(element);
     }
   }
   createStyles(element) {
-    element.style.cssText = `
-    height: ${this.height}px;
-    width: ${this.width}px;
-    background: ${this.bg};
-    font-size: ${this.fontSize}px;
+    if (this.objCss) {
+      for (let key in this.objCss) {
+        element.style.cssText += `${key}: ${this.objCss[key]}`;
+      }
+    }
+    element.style.cssText += `
+      height: ${this.height}px;
+      width: ${this.width}px;
+      background: ${this.bg};
+      font-size: ${this.fontSize}px;
   `;
+  }
+  listener(key) {
+    let elem = document.getElementsByClassName(`${this.selector}`)[0];
+    let top = parseInt(window.getComputedStyle(elem).top);
+    let bottom = parseInt(window.getComputedStyle(elem).bottom);
+    let left = parseInt(window.getComputedStyle(elem).left);
+    let right = parseInt(window.getComputedStyle(elem).right);
+
+    switch (key) {
+      case 'ArrowUp':
+        elem.style.top = top + 10 + 'px';
+        break;
+      case 'ArrowDown':
+        elem.style.bottom = bottom + 10 + 'px';
+        break;
+      case 'ArrowLeft':
+        elem.style.left = left + 10 + 'px';
+        break;
+      case 'ArrowRight':
+        elem.style.right = right + 10 + 'px';
+        break;
+    }
   }
 }
 
-const newElem = new DomElement('        #paragraph', 'auto', 500, '#bbe46a', 30);
-const newElem2 = new DomElement('        .block', 'auto', 300, 'rgb(228 106 222 / 50%)', 16);
-
-newElem.createElement();
-newElem2.createElement();
+document.addEventListener('DOMContentLoaded', function () {
+  const text = ['Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores, quibusdam.'];
+  const objCss = {
+    position: 'absolute',
+  };
+  const square = new DomElement('.square', 100, 100, '#bbe46a', 30, '', objCss);
+  square.createElement();
+  document.addEventListener('keydown', function (e) {
+    square.listener(e.key);
+  });
+});
